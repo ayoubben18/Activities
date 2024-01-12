c0143cdaa1b437530a2357c853065e925685024d
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-01-12 21:49:36 +0100

    walking skeleton client-side

--- API/Program.cs
+++ API/Program.cs
@@ -15,10 +15,21 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(
+        "CorsPolicy",
+        policy =>
+        {
+            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        }
+    );
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. or often caled middleware
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
